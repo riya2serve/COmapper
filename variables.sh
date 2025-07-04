2716bc26751ba7f60c2342db83e1d6f1cb6e1a32
Riya Rampalli <riyarampalli@MacBookPro.lan>
2025-07-04 18:24:16 -0400

    Added new data processing pipeline with SAM-to-BAM conversion

--- variables.sh
+++ variables.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # the home directory of git repository
-dir_home=""
+dir_home=/Users/riyarampalli/hacks/COmapper
 
 # the number of available threads
-n_parallel=
+n_parallel=4
 
 # The name of input file (fq.gz)
-input=""
+input=sim_lyrata.fq.gz
 
 # Location of minimap reference of TAIR10.
 #ref=/path/to/TAIR10.mmi
-ref=""
+ref=A_thaliana.mmi
 
 # Location of masksam.awk. 
-awkloc=resources/masksam.awk
+awkloc=resources/masksam_Q14.awk
 
 export dir_home
 export n_parallel
